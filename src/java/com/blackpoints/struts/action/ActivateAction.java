1e7dc5f2fddb56f78e1ac237868c29651ba7a8ae
hoangkianh <hoangkianh31592@gmail.com>
2014-01-28 11:23:13 +0700

    send welcome email

--- src/java/com/blackpoints/struts/action/ActivateAction.java
+++ src/java/com/blackpoints/struts/action/ActivateAction.java
@@ -2,11 +2,16 @@ package com.blackpoints.struts.action;
 
 import com.blackpoints.classes.User;
 import com.blackpoints.dao.UserDAO;
+import com.blackpoints.util.SendingEmail;
+import java.util.Locale;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.apache.struts.Globals;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.apache.struts.util.MessageResources;
 
 /**
  *
@@ -37,8 +42,23 @@ public class ActivateAction extends org.apache.struts.action.Action {
             if (u.isActivated()) {
                 return mapping.findForward("activateRedirect");
             }
-            
+
             if (salt.equals(u.getSalt()) && userDAO.activateUser(email)) {
+                // send reactive email
+                HttpSession session = request.getSession(true);
+                MessageResources mr = MessageResources.getMessageResources("com.blackpoints.struts.ApplicationResource");
+                Locale locale = (Locale) session.getAttribute(Globals.LOCALE_KEY);
+                String from = mr.getMessage(locale, "emailconfig.from");
+                String password = mr.getMessage(locale, "emailconfig.password");
+                String subject = mr.getMessage(locale, "emailconfig.welcome.subject", u.getDisplayName());
+                StringBuilder body = new StringBuilder(mr.getMessage(locale, "emailconfig.welcome.body", u.getDisplayName()));
+                body.append(mr.getMessage(locale, "emailconfig.welcome.link"));
+                body.append(mr.getMessage(locale, "emailconfig.help"));
+                body.append(mr.getMessage(locale, "emailconfig.sign"));
+
+                if (!SendingEmail.sendEmail(from, password, email, subject, body.toString())) {
+                    return mapping.findForward("sendingEmailFailure");
+                }
                 return mapping.findForward("activateSuccess");
             }
         }
